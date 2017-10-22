d868b538e131d55fc9b1ee84556f187a9e5425eb
Bhuvan M S <msbhuvanbhuvi@gmai.com>
2017-10-23 03:42:13 +0530

    solution to question 1.

--- linear_algebra/gauss_elimination.py
+++ linear_algebra/gauss_elimination.py
@@ -7,7 +7,7 @@ Assignment 1
 Implement the Gauss elimination algorithm for computing the solution to a linear system of equations Ax = b
 Requirements
 
-    1. The program should take as argument a single file name. The file will contain the pickle'd numpy matrix A and vector b
+    1. The program should take as argument a single file name. The file will contain the pickle'd numpy matrix A and vector b. (Changed to augmented matrix - [A b])
     2. You need to implement the algorithm from scratch, not use a numpy routine
     3. The output should be a numpy array containing the solution x
     4. Your code should detect whether the system of equations is consistent or not
@@ -22,9 +22,9 @@ The submission type is set to Online Text. Simply paste the github URL to your p
 """
 
 import argparse
-import math
 import numpy as np
 import pickle
+import copy
 
 def get_args():
     """
@@ -34,25 +34,122 @@ def get_args():
     parser = argparse.ArgumentParser(description="Gauss elimination algorithm for computing the solution to a "
                                                  "linear system of equations Ax = b.")
     parser.add_argument('--input_file',
-                        help='Specify the path to the pickle file containing a tuple containing matrix A and vector b.')
+                        help='Specify the path to the pickle file containing the augmented matrix [A b]')
     parser.add_argument('--output_file',
-                        help='Specify the path to the which the solution vector x needs to saved.')
+                        default=None,
+                        help='Specify the path to the which the solution vector x needs to saved. '
+                             'Default: Saves the output in the current directory.')
     args = parser.parse_args()
     return args
 
+def _get_row_echelon_form(A):
+    """
+    Convert to row echelon form for the augmented matrix.
+    Args:
+        A: Augmented matrix [A b]
 
-def gauss_elimination(A, b):
+    """
+    A = copy.deepcopy(A)
+    m, n = A.shape
+    # n - 1 because we don't need to consider the augmented column b.
+    for k in range(min(m, n - 1)):
+        # Find the pivot and swap rows.
+        i_max = k + np.argmax(np.abs(A[k:, k]))
+        # This catches divide by zero.
+        if A[i_max, k] == 0:
+            continue
+            """
+            print "The highest pivot is 0."
+            if A[i_max, n - 1] == 0:
+                print "The corresponding b is also 0. Hence, the system could still be consistent."
+                continue
+            else:
+                raise ValueError("Inconsistent system of equations: The sub-matrix is singular.")
+            """
+
+        # Swap axes with the pivot i_max row.
+        A[[i_max, k]] = A[[k, i_max]]
+
+        # For all rows below pivot.
+        for i in range(k + 1, m):
+            f = A[i, k] / A[k, k]
+
+            # For all elements in the current row.
+            for j in range(k, n):
+                A[i, j] = A[i, j] - A[k, j] * f
+
+    return A
+
+def _back_substitution(T):
+    """
+    Perform back substitution on augmented matrix.
+    Here it is assumed that the size of the array is (m, m +1).
+
+    """
+    m, n = T.shape
+    if n != m + 1:
+        raise ValueError("Backsubstituion must get array of shape : m, m +1")
+
+    x = np.zeros(m)
+    for i in reversed(range(m)):
+        k = np.dot(x, T[i,:-1])
+        # v = T[i, n - 1] / T[i, i]
+        x[i] = (T[i, n - 1] - k) / T[i, i]
+
+    return x
+
+
+def gauss_elimination(Ab):
     """
     The Gauss Elimination algorithm to find solution to the system of linear equation Ax = b.
-    Returns the solution vector x.
+    Args:
+        Ab: The augmented matrix [A b]
+    Returns:
+        The solution vector x.
 
     """
-    pass
+    # Copy to keep the function immutable.
+    Ab = copy.deepcopy(Ab)
+    Ab = Ab.astype(np.float64)
+
+    R = _get_row_echelon_form(Ab)
+    print "Row echelon augmented matrix: \n%s\n" % R
+
+    # Determine if the system of equations are inconsistent.
+    m, n = R.shape
+    x_len = n - 1
+    for free_i in reversed(range(m)):
+        if R[free_i, n - 1 - 1] != 0:
+            break
+
+        if R[free_i, n - 1] != 0:
+            raise ValueError("Inconsistent system of equations: no solutions exist.")
+
+    # Slice out the free variable rows.
+    # This reduces the computation for back substitution.
+    T = R[:free_i + 1, :]
+    print "Row echelon free variables sliced augmented matrix: \n%s\n" % T
+    #print free_i
+
+    # Slice out the columns on the right.
+    # This reduces the computation for back substitution.
+    S = T[:, :free_i + 1]
+    b_row = T[:, n - 1]
+    b_row1 = b_row.reshape((len(b_row), 1))
+    S = np.hstack((S, b_row1))
+    print "Row echelon free variables sliced, square converted augmented matrix: \n%s\n" % S
+
+    # Back substitution.
+    x_unique = _back_substitution(S)
+    x = np.pad(x_unique, (0, x_len - len(x_unique)), 'constant')
+    return x
 
 
 if __name__ == "__main__":
     args = get_args()
-    # b is assumed to be the column vector.
-    A, b = pickle.load(open(args.input_file))
-    x = gauss_elimination(A, b)
+    Ab = pickle.load(open(args.input_file))
+    x = gauss_elimination(Ab)
+    print "The solution is: %s" % x
+    if not args.outout_file:
+        args.outout_file = "./x.pickle"
     pickle.dump(x, open(args.outout_file, "w"))
